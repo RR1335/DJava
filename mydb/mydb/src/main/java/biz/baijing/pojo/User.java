b6518e20d7257d9fe32685d74d47bd8c9205930f
RR1335 <odyli@126.com>
2025-05-08 05:06:25 -0400

    update lombok

--- mydb/mydb/src/main/java/biz/baijing/pojo/User.java
+++ mydb/mydb/src/main/java/biz/baijing/pojo/User.java
@@ -14,66 +14,46 @@ public class User {
     private Short gender;
     private String phone;
 
-//    public User() {
-//
-//    }
-//
-//    public User(Integer id, String username, Short age, Short gender, String phone) {
-//        this.id = id;
-//        this.username = username;
-//        this.age = age;
-//        this.gender = gender;
-//        this.phone = phone;
-//    }
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public Short getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Short age) {
-//        this.age = age;
-//    }
-//
-//    public Short getGender() {
-//        return gender;
-//    }
-//
-//    public void setGender(Short gender) {
-//        this.gender = gender;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", username='" + username + '\'' +
-//                ", age=" + age +
-//                ", gender=" + gender +
-//                ", phone='" + phone + '\'' +
-//                '}';
-//    }
+/*    public User() {
+
+    }
+
+    public User(Integer id, String name, Short age, Short gender, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public void setGender(Short gender) {
+        this.gender = gender;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", phone='" + phone + '\'' +
+                '}';
+    }*/
 }
