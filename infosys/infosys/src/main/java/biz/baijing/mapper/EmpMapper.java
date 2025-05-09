a4626943a2764911b4aba0039d8a741cc28cf31e
RR1335 <odyli@126.com>
2025-05-09 00:28:03 -0400

    mybatis

--- infosys/infosys/src/main/java/biz/baijing/mapper/EmpMapper.java
+++ infosys/infosys/src/main/java/biz/baijing/mapper/EmpMapper.java
@@ -1,12 +1,45 @@
 package biz.baijing.mapper;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
+import biz.baijing.pojo.Emp;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface EmpMapper {
 
+    // $ and # , # 预编译
     @Delete("delete from emp where id = #{id}")
     public int delete(Integer id);
+
+
+    // 获取返回的主键值
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    // insert into 插入数据的 emp是数据库的命名（下划线命名）；values 是（Emp emp）变量的 Bean 类的命名（小驼峰命名）
+    @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
+            "values (#{username},#{name},#{gender},#{image},#{job},#{entrydate},#{deptId},#{createTime},#{updateTime})")
+    public int insert(Emp emp);
+
+
+    @Update("update emp set username=#{username},name=#{name},gender=#{gender},image=#{image}, " +
+            "job=#{job},entrydate=#{entrydate},dept_id=#{deptId},update_time=#{updateTime} where id = #{id}")
+    public void update(Emp emp);
+
+
+    @Results({
+            @Result(column  = "dept_id",property = "deptId"),
+            @Result(column = "create_time" , property = "createTime"),
+            @Result(column = "update_time",property = "updateTime")
+    })
+    @Select("select username,name,gender,job,entrydate,dept_id,update_time from emp where id=#{id}")
+    public Emp getEmpById(Integer id);
+
+
+//    @Select("select * from emp where id=#{id}")
+//    @Select("select id, username, password, name, gender, image, job, entrydate, dept_id, create_time, update_time from emp where id=#{id}")
+    @Select("select id, username, password, name, gender, image, job, entrydate, dept_id deptId, create_time createTime, update_time updateTime from emp where id=#{id}")
+    public Emp getEmpById01(Integer id);
+
+    // Mybatis 驼峰命名的开关 , properties 文件中配置 mybatis.configuration.map-underscore-to-camel-case=true
+    @Select("select * from emp where id=#{id}")
+    public Emp select(Integer id);
 
 }
--- infosys/infosys/src/test/java/biz/baijing/InfosysApplicationTests.java
+++ infosys/infosys/src/test/java/biz/baijing/InfosysApplicationTests.java
@@ -1,9 +1,13 @@
 package biz.baijing;
 
 import biz.baijing.mapper.EmpMapper;
+import biz.baijing.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class InfosysApplicationTests {
@@ -11,10 +15,76 @@ class InfosysApplicationTests {
     @Autowired
     private EmpMapper empMapper;
 
-    @Test
+/*    @Test
     public void testDelete() {
         int delete = empMapper.delete(16);
         System.out.println(delete);
+    }*/
+
+
+/*    @Test
+    public void testInsert() {
+        // 构造员工对象
+        Emp emp = new Emp();
+        // 员工对象属性赋值
+        emp.setUsername("ruhua");
+        emp.setName("如花");
+        emp.setImage("12.png");
+        emp.setGender((short) 1);
+        emp.setJob((short) 1);
+        emp.setEntrydate(LocalDate.of(2020, 11, 12));
+        emp.setDeptId(1);
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+
+        // 插入操作
+        empMapper.insert(emp);
+
+        // 输出主键ID
+        System.out.println(emp.getId());
+
+    }*/
+
+    @Test
+    public void testUpdate() {
+        // 构造员工对象
+        Emp emp = new Emp();
+        //更新数据员工的 id
+        emp.setId(15);
+        // 员工对象属性赋值
+        emp.setUsername("huoyanshan");
+        emp.setName("火焰山");
+        emp.setImage("12.png");
+        emp.setGender((short) 1);
+        emp.setJob((short) 1);
+        emp.setEntrydate(LocalDate.of(2023, 11, 12));
+        emp.setDeptId(1);
+        emp.setUpdateTime(LocalDateTime.now());
+
+        // 插入操作
+        empMapper.update(emp);
+
+
     }
+
+    @Test
+    public void testSelect() {
+        Emp emp = empMapper.getEmpById(19);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testSelect01() {
+        Emp emp = empMapper.getEmpById01(19);
+        System.out.println(emp);
+    }
+
+    // 开启 驼峰命名转换后
+    @Test
+    public void testSelect02() {
+        Emp emp = empMapper.select(15);
+        System.out.println(emp);
+    }
+
 
 }
